e6a8ee35e48c3ac7d207650cd9bd68a3af64345c
venky626 <venky626@a6dcb018-2199-11de-bf1e-7b5f7763e386>
2009-04-10 15:13:39 +0000

    baselined version for datastore

--- src/db_model.py
+++ src/db_model.py
@@ -1,20 +1,63 @@
 from google.appengine.ext import db
+import datetime
 
+class CountryLoader(Loader):
+  def __init__(self):
+    Loader.__init__(self, 'Country',
+                    [('name', str),
+                     ('misc', str)
+                     ])
 
+class ClubLoader(Loader):
+  def __init__(self):
+    Loader.__init__(self, 'Club',
+                    [('name', str),
+                     ('city', str),
+                     ('short_name', str),
+                     ('captain_name', str),
+                     ('owner', str),
+                     ('matches_total', int),
+                     ('matches_won', int),
+                     ('matches_lost', int),
+                     ('matches_tie', int),
+                     ('matches_cancelled', int),
+                     ('highest_score', int),
+                     ('avg_score', float),
+                     ('avg_opp_score', float),
+                     ])
+    
 class Club(db.Model):
     name = db.StringProperty()
-    captain = db.StringProperty()
     owner = db.StringProperty()
+    captain_name = db.StringProperty()    
     city = db.StringProperty()
-    total_matches = db.IntegerProperty()
-    wins = db.IntegerProperty()
-    losses = db.IntegerProperty()
-    ties = db.IntegerProperty()
-    canceled = db.IntegerProperty()
-    short_code = db.StringProperty()
+    matches_total = db.IntegerProperty()
+    matches_won = db.IntegerProperty()
+    matches_lost = db.IntegerProperty()
+    matches_tie = db.IntegerProperty()
+    matches_cancelled = db.IntegerProperty()
+    highest_score = db.IntegerProperty()
+    avg_score = db.FloatProperty()
+    avg_opp_score = db.FloatProperty()
+    short_name = db.StringProperty()
     
 class Country(db.Model):
     name = db.StringProperty()
+    misc = db.StringProperty()
+
+class PlayerLoader(Loader):
+    def __init__(self):
+      Loader.__init__(self, 'Player',
+                    [('name', str),
+                     ('dob', lambda x: datetime.datetime.strptime(x, '%m/%d/%y').date()),
+                     ('country_name', str),
+                     ('battype', str),
+                     ('bowltype', str),
+                     ('type', str),
+                     ('club_name', str),
+                     ('status', int),
+                     ('price', float),
+                     ])
     
 class Player(db.Model):
     name = db.StringProperty()
@@ -27,8 +70,8 @@ class Player(db.Model):
     bowltype = db.StringProperty()
     status = db.IntegerProperty()
     price = db.FloatProperty()
-    club_name = db.StringProperty()
     country_name = db.StringProperty()
+    club_name = db.StringProperty()
 
 class User(db.Model):
     id = db.StringProperty()
