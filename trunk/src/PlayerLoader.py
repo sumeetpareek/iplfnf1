12d4951b19bf6f1a8efc16a7f3a7121300254ec9
venky626 <venky626@a6dcb018-2199-11de-bf1e-7b5f7763e386>
2009-04-10 05:08:49 +0000

    sample loader

---
+++ trunk/src/PlayerLoader.py
@@ -0,0 +1,17 @@
+import datetime
+from google.appengine.ext import db
+import models
+
+class PlayerLoader(Loader):
+  def __init__(self):
+    Loader.__init__(self, 'Player',
+                    [('name', str),
+                     ('type', str),                     
+                     ('dob', lambda x: datetime.datetime.strptime(x, '%m/%d/%Y').date()),
+                     ('age', int),
+                     ('battype', str),
+                     ('bowltype', str),
+                     ('status', int),
+                     ('price', float)
+                     ])
+    
