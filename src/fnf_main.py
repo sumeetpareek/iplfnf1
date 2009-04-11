938188390e9387cba5144b5e91b56f60d89ce058
positivecharge@gmail.com <positivecharge@gmail.com@a6dcb018-2199-11de-bf1e-7b5f7763e386>
2009-04-11 07:07:41 +0000

    base version ready!!

--- src/fnf_main.py
+++ src/fnf_main.py
@@ -5,10 +5,21 @@ import wsgiref.handlers
 from google.appengine.ext import webapp
  
 # OpenSocial Gifts imports
-import test_data 
+import test_data
+import dummy
  
 # Map URLs to request handler classes
-application = webapp.WSGIApplication([('/admin', test_data.AdminServer)],
+application = webapp.WSGIApplication([('/admin', test_data.AdminServer),
+                                      ('/data/dummy_user',dummy.dummyServer),
+                                      ('/data/dummy_fact',dummy.dummyServer),
+                                      ('/data/dummy_fact_player',dummy.dummyServer),
+                                      ('/data/dummy_fact_club',dummy.dummyServer),
+                                      ('/data/dummy_fact_vote',dummy.dummyServer),
+                                      ('/data/player_update_age',dummy.dummyServer),
+                                      ('/data/player_update_status',dummy.dummyServer),
+                                      ('/data/player_update_clubref',dummy.dummyServer),
+                                      ('/data/player_update_countryref',dummy.dummyServer)
+                                      ],
                                      debug=True)
  
 # Fire it up!
