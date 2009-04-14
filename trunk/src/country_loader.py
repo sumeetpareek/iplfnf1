cc2730507a1083bcb71768cbf851fe2b0aa5dad4
positivecharge@gmail.com <positivecharge@gmail.com@a6dcb018-2199-11de-bf1e-7b5f7763e386>
2009-04-14 21:29:05 +0000

    deleting redundant files after the code refactoring

--- trunk/src/country_loader.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import db
-
-class CountryLoader(Loader):
-  def __init__(self):
-    Loader.__init__(self, 'Country',
-                    [('name', str)
-                     ])
-    
