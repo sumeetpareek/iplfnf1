37a6e923aebd73fe7c41752ee2eb726ea4de2404
positivecharge@gmail.com <positivecharge@gmail.com@a6dcb018-2199-11de-bf1e-7b5f7763e386>
2009-04-15 04:25:47 +0000

    redesign of fact functionalities (still buggy fact get)

--- src/db_fact.py
+++ src/db_fact.py
@@ -3,21 +3,11 @@ from db_fantasy import *
 from db_general import *
 import datetime
 
-class Fact(db.Model):
+class Fact(db.Expando):
     creator = db.ReferenceProperty(User)
     content = db.TextProperty()
     total_vote_up = db.IntegerProperty()
     total_vote_down = db.IntegerProperty()
-    timestamp = db.TimeProperty(auto_now_add=True)
-
-class Fact_Player(db.Model):
-    fact = db.ReferenceProperty(Fact)
-    player = db.ReferenceProperty(Player)
-    timestamp = db.TimeProperty(auto_now_add=True)
-    
-class Fact_Club(db.Model):
-    fact = db.ReferenceProperty(Fact)
-    club = db.ReferenceProperty(Club)
     timestamp = db.TimeProperty(auto_now_add=True)
         
 class Fact_Vote(db.Model):
